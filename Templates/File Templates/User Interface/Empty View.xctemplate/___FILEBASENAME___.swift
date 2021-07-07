62ce3ccf9bca8050bf4a6720552f1c1abcab6aa6
nkukushkin <nkukushkin@me.com>
2021-07-07 11:07:29 +0300

    Add templates

---
+++ Templates/File Templates/User Interface/Empty View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,23 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+  // MARK: View
+
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+// MARK: - Previews
+
+#if DEBUG
+struct ___FILEBASENAMEASIDENTIFIER____Previews: PreviewProvider {
+  // MARK: PreviewProvider
+
+  static var previews: some View {
+    ___FILEBASENAMEASIDENTIFIER___()
+  }
+}
+#endif
